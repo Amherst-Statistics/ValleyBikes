061628c98f8fa6f889ed386b2760cfad37a900ec
Shukry Zablah <shukryzablah@gmail.com>
2020-02-02 18:59:57 -0500

    changed name from ValleyBikes to valleybike

---
+++ R/valleybike.R
@@ -0,0 +1,9 @@
+#' valleybike: A package for exploring data from Pioneer Valley bikeshare.
+#'
+#' This package provides an easy way to start exploring data from
+#' the Valley Bike initiative through the `stations` and `routes`
+#' datasets.
+#'
+#' @docType package
+#' @name valleybike
+NULL
